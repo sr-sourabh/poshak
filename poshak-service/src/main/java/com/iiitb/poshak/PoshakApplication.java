02871653c5e525fc68d6ba3ca4651d0879123c94
sr-sourabh <shourabh@glabbr.com>
2021-03-30 20:04:37 +0530

    ic

---
+++ poshak-service/src/main/java/com/iiitb/poshak/PoshakApplication.java
@@ -0,0 +1,13 @@
+package com.iiitb.poshak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoshakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoshakApplication.class, args);
+    }
+
+}
---
+++ poshak-service/src/test/java/com/iiitb/poshak/PoshakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iiitb.poshak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoshakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
