bb475c3e9bea939a755a27fd9597c947d1aaddfe
Ayush Gaurav <ayush.gaurav2014@gmail.com>
2021-04-04 10:48:11 +0530

    Scroll to top feature added

--- poshak-ui/src/components/HeroSection/index.js
+++ poshak-ui/src/components/HeroSection/index.js
@@ -36,7 +36,13 @@ const  HeroSection = () => {
                     onMouseEnter={onHover} 
                     onMouseLeave={onHover}
                     primary='true'
-                    dark='true'>
+                    dark='true'
+                    smooth={true} 
+                    duration={500} 
+                    spy={true} 
+                    exact='true' 
+                    offset={-80}
+                    >
                         Get started {hover ? <ArrowForward />:<ArrowRight />}
                     </Button>
                 </HeroBtnWrapper>
--- poshak-ui/src/pages/signin.js
+++ poshak-ui/src/pages/signin.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import SignIn from '../components/Signin'
+import ScrollToTop from '../components/ScrollToTop'
 
 const SigninPage = () => {
     return (
-        <div>
-            <h1>Hello signin</h1>
-        </div>
+        <>
+            <ScrollToTop />
+            <SignIn />
+        </>
     )
 }
 
