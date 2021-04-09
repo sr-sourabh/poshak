405d546bb98c24d594277138b199e873adaff766
Ayush Gaurav <ayush.gaurav2014@gmail.com>
2021-04-10 01:17:55 +0530

    Dashboard sidebar data added

--- poshak-ui/src/App.js
+++ poshak-ui/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Home from './pages'
 import SigninPage from './pages/signin';
+import DashboardPage from './pages/dashboard'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Switch>
         <Route path='/' component = {Home} exact/>
         <Route path='/signin' component = {SigninPage} exact/>
+        <Route path='/dashboard' component = {DashboardPage} exact/>
       </Switch>
     </Router>
   );
---
+++ poshak-ui/src/components/dashboardSideBar/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DashboardSidebar = () => {
+    return (
+        <div>
+            Sidebar
+        </div>
+    )
+}
+
+export default DashboardSidebar ;
