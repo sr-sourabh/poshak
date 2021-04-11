e1b90c7b601e8f43dec69c658c3301999290213d
Ayush Gaurav <ayush.gaurav2014@gmail.com>
2021-04-11 12:37:16 +0530

    search bar actual data rendered

--- poshak-ui/src/components/searchcomponent/index.js
+++ poshak-ui/src/components/searchcomponent/index.js
@@ -1,3 +1,108 @@
+import { Input } from "antd"
+import { useCombobox } from "downshift"
+import React, { useEffect, useState } from "react"
+import "./search.css"
+import styled from 'styled-components'
+import fooddata from './fooddata.json'
+
+
+function SearchComp() {
+  const [inputItems, setInputItems] = useState([])
+  const [users, setUsers] = useState([])
+  const [singleUser, setSingleUser] = useState("")
+
+  // useEffect(() => {
+  //   fetch("https://jsonplaceholder.typicode.com/users")
+  //     .then((response) => response.json())
+  //     .then((data) => setUsers(data))
+  // }, [])
+  
+  // useEffect(() => {
+  //   fetch(`${fooddata}.json`)
+  //     .then((response) => response.json())
+  //     .then((data) => setUsers(data))
+  // }, [])
+
+  useEffect(() => {
+     setUsers(fooddata)
+  }, [])
+ 
+  console.log(fooddata);
+
+  const {
+    isOpen,
+    getMenuProps,
+    getInputProps,
+    getComboboxProps,
+    highlightedIndex,
+    getItemProps,
+  } = useCombobox({
+    items: inputItems,
+    onInputValueChange: ({ inputValue }) => {
+      setInputItems(
+        users.filter((item) =>
+          item.food.toLowerCase().startsWith(inputValue.toLowerCase())
+        )
+      )
+    },
+  })
+
+
+  return (
+    <div className="App">
+      <h1>Current Item: {singleUser}</h1>
+      <div {...getComboboxProps()}>
+        <Input
+          {...getInputProps()}
+          placeholder="Search"
+          enterButton="Search"
+        //   value={singleUser}
+          type="text"
+          size="large"
+        />
+        
+      </div>
+      <ul {...getMenuProps()}>
+        {isOpen &&
+          inputItems.map((item, index) => (
+            <span
+              key={item.id}
+              {...getItemProps({ item, index })}
+              onClick={() => setSingleUser(item.food)}
+            >
+              <li
+                style={highlightedIndex === index ? { background: "#ede" } : {}}
+              >
+                <h4>{item.food}</h4>
+              </li>
+            </span>
+          ))}
+      </ul>
+      <h1> <label for="quant">Quantity:</label><br /> </h1>
+      <input 
+        type="text"
+        id="quant"
+        name="quant"    
+        /> <br /><br />
+        <button type='submit' className="btn">Log</button>
+    </div>
+  )
+}
+
+export default SearchComp;
+
+
+
+
+
+
+
+
+
+
+
+
+
 // import React, {useEffect, useState} from 'react'
 // import './search.css'
 // import {useCombobox} from 'downshift'
@@ -52,83 +157,4 @@
 // export default searchComp ;
 
 
-import { Input } from "antd"
-import { useCombobox } from "downshift"
-import React, { useEffect, useState } from "react"
-import "./search.css"
-import styled from 'styled-components'
 
-
-
-function SearchComp() {
-  const [inputItems, setInputItems] = useState([])
-  const [users, setUsers] = useState([])
-  const [singleUser, setSingleUser] = useState("")
-
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((data) => setUsers(data))
-  }, [])
-
-  const {
-    isOpen,
-    getMenuProps,
-    getInputProps,
-    getComboboxProps,
-    highlightedIndex,
-    getItemProps,
-  } = useCombobox({
-    items: inputItems,
-    onInputValueChange: ({ inputValue }) => {
-      setInputItems(
-        users.filter((item) =>
-          item.name.toLowerCase().startsWith(inputValue.toLowerCase())
-        )
-      )
-    },
-  })
-
-
-  return (
-    <div className="App">
-      <h1>Current Item: {singleUser}</h1>
-      <div {...getComboboxProps()}>
-        <Input
-          {...getInputProps()}
-          placeholder="Search"
-          enterButton="Search"
-        //   value={singleUser}
-          type="text"
-          size="large"
-        />
-        
-      </div>
-      <ul {...getMenuProps()}>
-        {isOpen &&
-          inputItems.map((item, index) => (
-            <span
-              key={item.id}
-              {...getItemProps({ item, index })}
-              onClick={() => setSingleUser(item.name)}
-            >
-              <li
-                style={highlightedIndex === index ? { background: "#ede" } : {}}
-              >
-                <h4>{item.name}</h4>
-              </li>
-            </span>
-          ))}
-      </ul>
-      <h1> <label for="quant">Quantity:</label><br /> </h1>
-      <input 
-        type="text"
-        id="quant"
-        name="quant"    
-        /> <br /><br />
-        <button type='submit' className="btn">Sign In</button>
-    </div>
-  )
-}
-
-export default SearchComp;
