899c0df1401fb360f813fc16060c8adf7c1005aa
Ayush Gaurav <ayush.gaurav2014@gmail.com>
2021-04-15 18:53:59 +0530

    dashboard charts added

--- poshak-ui/src/pages/dashboard.js
+++ poshak-ui/src/pages/dashboard.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import BlogOverview from '../components/Dashboard/index'
 import DashboardSidebar from '../components/dashboardSideBar'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../components/Dashboard/shards-dashboard/styles/shards-dashboards.1.1.0.min.css";
+import "../css/overview.css";
 
 const DashboardPage = () => {
     return (
         <>
             <DashboardSidebar />
+            <div className="nav-expanded">
+                <BlogOverview />
+            </div>
+            
         </>
     )
 }
