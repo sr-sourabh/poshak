cbe58cff4b2895c240e7896d9d98685fd1183448
Ayush Gaurav <ayush.gaurav2014@gmail.com>
2021-04-17 01:17:08 +0530

    axios module added

--- poshak-ui/src/components/Signin/index.js
+++ poshak-ui/src/components/Signin/index.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, {useState} from 'react'
+import axios from 'axios';
 
 import {
     Container,
@@ -15,19 +16,46 @@ import {
 from './SigninElements'
 
 const SignIn = () => {
+
+    const [state , setState] = useState({
+        email : "",
+        password : "",
+        datarecived : ""
+    })
+
+    const handleChange = (e) => {
+
+    }
+
+    function handleSubmit(e) {
+        // alert(document.getElementById("email").value);
+        // alert(document.getElementById("password").value);
+        axios({
+            method: 'put',
+            url: 'http://localhost:8090/user/login',
+            data: {
+                "emailId": document.getElementById("email").value,
+                "password":document.getElementById("password").value
+            }
+        })
+        .then(res => alert(res.data ));
+        // console.log("ddsfsdf");
+        // console.log(state.datarecived);
+    }
+
     return (
         <>
           <Container>
               <FormWrap>
                   <Icon to='/'>Poshak</Icon>
                   <FormContent>
-                      <Form action='/overview'>
+                      <Form  >
                           <FormH1>Sign In to your account</FormH1>
                           <FormLabel htmlFor='for'>Email</FormLabel>
-                          <FormInput type='email' placeholder='example@email.com' required />
+                          <FormInput id="email" type='email' placeholder='example@email.com' required />
                           <FormLabel htmlFor='for'>Password</FormLabel>
-                          <FormInput type='password' placeholder='password' required />
-                          <FormButton type='submit'>Sign In</FormButton>
+                          <FormInput id="password" type='password' placeholder='password' required />
+                          <FormButton type='submit' onClick={handleSubmit} >Sign In</FormButton>
                           <Text>Forgot password</Text>
                       </Form>
                   </FormContent>
