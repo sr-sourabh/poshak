fcfccdae08c79684f5b6c8c32ec8a4f3981ce58d
Ayush Gaurav <ayush.gaurav2014@gmail.com>
2021-04-18 00:53:49 +0530

    route data transfer sinup to info done

--- poshak-ui/src/components/Signup/index.js
+++ poshak-ui/src/components/Signup/index.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {Router} from 'react-router-dom'
 
 import {
     Container,
@@ -14,22 +15,40 @@ import {
 } 
 from './SignupElements'
 
-const SignUp = () => {
+// const emailsign="";
+var passSign="";
+
+const SignUp = (props) => {
+
+    function handleSubmit(e) {
+        e.preventDefault();
+            if(document.getElementById("passSignup").value == document.getElementById("confirmPassSignup").value)
+            {
+                
+
+                // alert(document.getElementById("passSignup").value);
+                // alert(document.getElementById("confirmPassSignup").value);
+                document.location = `/userPersonalInfo?x=${btoa(document.getElementById("emailSignup").value)}&y=${btoa(document.getElementById("confirmPassSignup").value)}`;
+                
+                
+            }
+        }
+    
     return (
         <>
           <Container>
               <FormWrap>
               <Icon to='/'>Poshak</Icon>
                   <FormContent>
-                      <Form action='/userPersonalInfo'>
+                      <Form >
                           <FormH1>Sign Up Today and start living a nourishing lifestyle</FormH1>
                           <FormLabel htmlFor='for'>Email</FormLabel>
-                          <FormInput type='email' placeholder='example@email.com' required />
+                          <FormInput id="emailSignup" type='email' placeholder='example@email.com' required />
                           <FormLabel htmlFor='for'>Password</FormLabel>
-                          <FormInput type='password' placeholder='enter password' required />
+                          <FormInput id="passSignup" type='password' placeholder='enter password' required />
                           <FormLabel htmlFor='for'>Confirm Password</FormLabel>
-                          <FormInput type='password' placeholder='confirm password' required />
-                          <FormButton type='submit'>Sign up</FormButton>
+                          <FormInput id="confirmPassSignup" type='password' placeholder='confirm password' required />
+                          <FormButton type='submit' onClick={handleSubmit}>Sign up</FormButton>
                       </Form>
                   </FormContent>
               </FormWrap>
--- poshak-ui/src/components/userPersonalInfo/index.js
+++ poshak-ui/src/components/userPersonalInfo/index.js
@@ -12,14 +12,27 @@ import {
     Text
 } from './userPersonalInfoElements'
 
+
+
 const UserPersonalInfo = () => {
+    function handleSubmit(e) {
+        e.preventDefault();
+        
+        var url = new URL(window.location.href);
+        var c1 = atob(url.searchParams.get(("x")));
+        var c2 = atob(url.searchParams.get(("y")));
+        console.log(c1);
+        console.log(c2);
+    
+    }
+    // action='/overview'
     return (
         <>
             <Container>
                 <FormWrap>
                     <Icon to='/'>Poshak</Icon>
                     <FormContent>
-                        <Form action='/overview'>
+                        <Form >
                             <FormH1>Enter the below details to complete your profile</FormH1>
                             <FormLabel htmlFor='for'>Name</FormLabel>
                             <FormInput type='text' placeholder='enter your full name' required />
@@ -27,7 +40,7 @@ const UserPersonalInfo = () => {
                             <FormInput type='float' placeholder='enter height in centimeters' required />
                             <FormLabel htmlFor='for'>Weight</FormLabel>
                             <FormInput type='float' placeholder='enter weight in kilograms' required />
-                            <FormButton type='submit'>Submit Info</FormButton>
+                            <FormButton type='submit' onClick={handleSubmit}>Submit Info</FormButton>
                         </Form>
                     </FormContent>
                 </FormWrap>
