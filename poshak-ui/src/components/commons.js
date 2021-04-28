c335f5ab5e21aa29dc6fda54720345e8ad09be99
sr-sourabh <shourabh@glabbr.com>
2021-04-28 10:29:13 +0530

    kakfa with trainer goals

--- poshak-ui/src/components/commons.js
+++ poshak-ui/src/components/commons.js
@@ -1,6 +1,6 @@
 const Commons = {
-    //POSHAK_SERVICE: 'http://localhost:8090',
-    POSHAK_SERVICE: 'https://poshak-service-sr-sourabh.cloud.okteto.net',
-    TOPIC: 'm95de23k-poshak1'
+    POSHAK_SERVICE: 'http://localhost:8090',
+    //POSHAK_SERVICE: 'https://poshak-service-sr-sourabh.cloud.okteto.net',
+    TOPIC: 'm95de23k-poshak2'
 }
 export default Commons
